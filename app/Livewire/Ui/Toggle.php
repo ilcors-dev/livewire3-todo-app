f0493b4b3d2b1d45be56c48d498c763571115188
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-03 20:47:25 +0200

    feat: toggle component

---
+++ app/Livewire/Ui/Toggle.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire\Ui;
+
+use Livewire\Component;
+
+class Toggle extends Component
+{
+    public $size = 'md';
+
+    public $sizeClasses = [
+        'sm' => ['wrapper' => 'h-4 w-8', 'circle' => 'h-2.5 w-2.5 checked:translate-x-3.5'],
+        'md' => ['wrapper' => 'h-5 w-9', 'circle' => 'h-3.5 w-3.5 checked:translate-x-3.5'],
+        'lg' => ['wrapper' => 'h-6 w-11', 'circle' => 'h-4 w-4 checked:translate-x-5 left-0.5'],
+        'xl' => ['wrapper' => 'h-7 w-12', 'circle' => 'h-5 w-5 checked:translate-x-5 left-0.5']
+    ];
+
+    public $class = '';
+
+    public $checked = false;
+
+    public function render()
+    {
+        return view('livewire.ui.toggle');
+    }
+}
---
+++ resources/views/livewire/ui/toggle.blade.php
@@ -0,0 +1,11 @@
+<div class="{{ $class }} flex w-10 items-center">
+    <label tabindex="-1" class="group relative flex select-none items-center">
+        <input
+            class="{{ $sizeClasses[$size]['circle'] }} peer absolute inset-y-0 left-0.5 mx-0.5 my-auto translate-x-0 transform cursor-pointer appearance-none rounded-full border-0 bg-black shadow outline-none transition duration-200 ease-in-out checked:bg-none checked:text-white focus:outline-none dark:bg-white"
+            type="checkbox" {{ $checked ? 'checked' : '' }} wire:model="checked" />
+
+        <div
+            class="{{ $sizeClasses[$size]['wrapper'] }} block cursor-pointer rounded-full border-2 border-black transition duration-100 ease-in-out focus:outline-none focus:outline-1 dark:border-white">
+        </div>
+    </label>
+</div>
