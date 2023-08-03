4e7c87e45bd68422afd321176fda1c2cd3dcc69b
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-03 21:55:28 +0200

    feat: installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./app/Livewire/**/*.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
