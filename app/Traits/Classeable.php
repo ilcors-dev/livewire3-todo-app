6448b6661eb0bb4b44de89d7cc29602d9bcf5e34
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-06 21:52:33 +0200

    fix: use parameter

--- app/Traits/Classeable.php
+++ app/Traits/Classeable.php
@@ -16,9 +16,6 @@ trait Classeable
      */
     public function mergeClasses(string $class): void
     {
-        $this->class = TailwindMerge::instance()->merge(
-            $this->class,
-            "h-10 w-full border border-gray-300 rounded-md focus:ring-black focus:outline-none focus:ring-2 focus:ring-offset-2 px-3 py-2"
-        );
+        $this->class = TailwindMerge::factory()->make()->merge($this->class, $class);
     }
 }
