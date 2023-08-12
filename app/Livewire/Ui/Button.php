ecd8e6ba310e09b6fefb02267f227a3675690b5a
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-12 21:16:16 +0200

    feat: button component with variants & icons

---
+++ app/Livewire/Ui/Button.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire\Ui;
+
+use Livewire\Component;
+use App\Traits\HtmlAttributes;
+
+class Button extends Component
+{
+    use HtmlAttributes;
+
+    protected array $variants = [
+        'primary' => 'bg-black text-white hover:bg-black/80 focus:ring-black',
+        'secondary' => 'bg-gray-200 text-black hover:bg-gray-300 focus:ring-gray-300 ',
+        'danger' => 'bg-red-500 text-white hover:bg-red-600 focus:ring-red-600',
+        'success' => 'bg-green-500 text-white hover:bg-green-600 focus:ring-green-600',
+        'outline' => 'border border-black text-black hover:bg-gray-50 focus:ring-black',
+        'link' => 'text-black hover:underline focus:ring-black',
+    ];
+
+    public string $variant = 'primary';
+
+    public ?string $label;
+
+    public ?string $leftIcon;
+
+    public ?string $rightIcon;
+
+    public function render()
+    {
+        return view('livewire.ui.button');
+    }
+}
---
+++ resources/views/livewire/ui/button.blade.php
@@ -0,0 +1,13 @@
+<button {{ $this->attributes() }}
+    class="{{ $this->mergeClasses($class, $this->variants[$variant], 'px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 inline-flex items-center space-x-1') }}">
+
+    @if ($leftIcon)
+        @svg($leftIcon, ['class' => 'h-4 w-4'])
+    @endif
+
+    <span>{{ $label }}</span>
+
+    @isset($rightIcon)
+        @svg($rightIcon, ['class' => 'h-4 w-4'])
+    @endisset
+</button>
