e5ce979368edb95ce822b2fb4051a4c7c666bfee
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-20 18:29:48 +0200

    feat: route to clear posts

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', \App\Livewire\SignIn::class)->name('auth.sign-in');
 Route::get('/sign-up', \App\Livewire\SignUp::class)->name('auth.sign-up');
 
+// Route::get('/posts/create', \App\Livewire\Posts\Create::class)->name('posts.create');
+
+Route::get('clear-posts', function () {
+    \App\Models\Post::truncate();
+
+    return redirect()->route('home');
+})->name('clear-posts');
+
 Route::middleware('auth')->group(function () {
     Route::get('/sign-out', function () {
         auth()->logout();
