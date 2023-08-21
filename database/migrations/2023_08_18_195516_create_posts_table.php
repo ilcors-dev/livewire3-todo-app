391b6125dbda8bc2dad7cbcb14328c804a281f37
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-21 23:02:43 +0200

    refactor: added cascade actions on sql

--- database/migrations/2023_08_18_195516_create_posts_table.php
+++ database/migrations/2023_08_18_195516_create_posts_table.php
@@ -15,9 +15,9 @@ return new class extends Migration {
         Schema::create('posts', function (Blueprint $table) {
             // $table->id();
             $table->ulid('id')->primary();
-            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->string('content', 500);
-            $table->foreignIdFor(Post::class)->nullable()->constrained();
+            $table->foreignIdFor(Post::class)->nullable()->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
